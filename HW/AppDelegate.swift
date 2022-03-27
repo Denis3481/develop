fb4a5010b5ebea51fd9aa7042b5abd3f5957be24
Denis3481 <denis.unix3481@yandex.ru>
2022-03-27 22:49:43 +0500

    добавил точку к комментарию класса

--- HW/AppDelegate.swift
+++ HW/AppDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {  //Объявляем класс
+class AppDelegate: UIResponder, UIApplicationDelegate {  //Объявляем класс.
 
     var window: UIWindow?
 
